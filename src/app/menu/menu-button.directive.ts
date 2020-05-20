6a72dacca4d8ffbcb2969c9cca65bea50ec1251f
Andy Chrzaszcz <andy9775@gmail.com>
2020-05-20 19:33:47 -0400

    refactor responsibilities of directives
    configure buttons to to handle opening/closing of attached menu panels
    and attach menu panels to buttons

--- src/app/menu/menu-button.directive.ts
+++ src/app/menu/menu-button.directive.ts
@@ -1,14 +1,89 @@
-import { Directive, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Directive, ElementRef, Input, ViewContainerRef } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { MenuPanelDirective } from './menu-panel.directive';
 
+/*
+  TODO
+  - on hover - if another button is open, close it and open this one (will
+    backdrop get in the way?)
+
+  ---------
+
+  backdrop changes the background color (to focus on opened content) and it
+  prevents mouse enter events from hitting the other buttons.
+
+  approaches to 'click-out-to-close':
+    - listen for document click events and filter out those which are in the
+      given template. This should consider parent menus - requires passing
+      events to close menus and keep others open
+        (rxjs fromEvent, filter and takeOne)
+    - listen for click events through out the parent chain with the menu bar
+      listening to the dom. Passing up/down the events through the menu tree.
+      (not ideal)
+    - what does mat-menu do?
+*/
 @Directive({
   selector: '[appMenuButton]',
   exportAs: 'appMenuButton',
   host: {
-    '(click)': 'onClick.emit()',
+    '(click)': 'onClick()',
   },
 })
 export class MenuButtonDirective {
-  @Output() onClick = new EventEmitter();
+  @Input('appMenuButton') templateRef: MenuPanelDirective;
 
-  constructor(public element: ElementRef<HTMLElement>) {}
+  private _overlayRef: OverlayRef;
+
+  constructor(
+    private _overlay: Overlay,
+    private _element: ElementRef,
+    private _viewContainer: ViewContainerRef
+  ) {}
+
+  onClick() {
+    !!this._overlayRef ? this._closeMenu() : this._openMenu();
+  }
+
+  private _openMenu() {
+    this._overlayRef = this._overlay.create({
+      positionStrategy: this._getOverlayPositionStrategy(),
+      // backdrop has a default style applied to it!!!
+      hasBackdrop: true,
+    });
+    const portal = new TemplatePortal(
+      this.templateRef.template,
+      this._viewContainer
+    );
+    this._overlayRef.attach(portal);
+
+    this._setCloseHandlers();
+  }
+
+  private _closeMenu() {
+    // TODO better clean up
+    this._overlayRef.detach();
+    this._overlayRef.dispose();
+    this._overlayRef = null;
+  }
+
+  private _getOverlayPositionStrategy() {
+    return this._overlay
+      .position()
+      .flexibleConnectedTo(this._element)
+      .setOrigin(this._element)
+      .withPositions([
+        {
+          originX: 'start',
+          originY: 'bottom',
+          overlayX: 'start',
+          overlayY: 'top',
+        },
+      ])
+      .withLockedPosition();
+  }
+
+  private _setCloseHandlers() {
+    this._overlayRef.backdropClick().subscribe(this._closeMenu.bind(this));
+  }
 }
--- src/app/menu/menu-panel.directive.ts
+++ src/app/menu/menu-panel.directive.ts
@@ -1,61 +1,14 @@
-import {
-  Directive,
-  TemplateRef,
-  ViewContainerRef,
-  Input,
-  HostListener,
-} from '@angular/core';
-import { MenuButtonDirective } from './menu-button.directive';
-import { Overlay } from '@angular/cdk/overlay';
-import { TemplatePortal } from '@angular/cdk/portal';
+import { Directive, TemplateRef } from '@angular/core';
 
 @Directive({
   selector: '[appMenuPanel]',
   exportAs: 'appMenuPanel',
 })
 export class MenuPanelDirective {
-  @Input('appMenuPanel') button: MenuButtonDirective;
-
-  constructor(
-    private overlay: Overlay,
-    private _viewContainer: ViewContainerRef,
-    private _template: TemplateRef<any>
-  ) {}
-
-  ngOnInit() {
-    this.button.onClick.subscribe(this.onClick.bind(this));
-  }
-
-  onClick() {
-    /*
-      TODO:
-        - vertically align sub-menu buttons
-        - close when clicking outside of opened panel
-     */
-    const strategy = this.overlay
-      .position()
-      .flexibleConnectedTo(this.button.element)
-      .setOrigin(this.button.element)
-      .withPositions([
-        {
-          originX: 'start',
-          originY: 'bottom',
-          overlayX: 'start',
-          overlayY: 'top',
-        },
-      ])
-      .withLockedPosition();
-    const overlay = this.overlay.create({
-      positionStrategy: strategy,
-      // backdrop has a style applied to it
-      hasBackdrop: true,
-    });
-
-    const portal = new TemplatePortal(this._template, this._viewContainer);
-    overlay.attach(portal);
-
-    overlay
-      .backdropClick()
-      .subscribe(() => console.log('TODO close menu if open'));
-  }
+  /*
+    TODO
+    - menu panel allows for sub-elements to be lined up horizontally - are we ok
+      with this?
+  */
+  constructor(public template: TemplateRef<any>) {}
 }
